221fc87d7b82472fa71eb556dba26b5f8b15f0bf
Deglas0 <Douglasmigsil@hotmail.com>
2023-09-04 23:12:34 -0300

    Primeiro commit
    Aqui está a primeira parte do projetinho de POO

---
+++ MediaEscolar/MediaEscolar/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaEscolar
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double[] Nota = new double[3];
+        public double Media;
+    }
+
+}
+
---
+++ MediaEscolar/MediaEscolar/Program.cs
@@ -0,0 +1,43 @@
+namespace MediaEscolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno n = new Aluno();
+            Console.WriteLine("Olá, vamos calcular a nota do aluno(a)?");
+            Console.Write("Digite o nome do Aluno: ");
+            n.Nome = Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Digite a nota: " + (i + 1));
+                n.Nota[i] = double.Parse(Console.ReadLine());
+                if (i < 1)
+                {
+                    if (n.Nota[i] > 30)
+                    {
+                        Console.WriteLine("numero invalido, porfavor digitar novaente");
+                    }
+                    else
+                    { Console.WriteLine($"nota {i} salva com sucesso..."); }
+                }
+                else
+                {
+                    if (n.Nota[i] > 35)
+                    {
+                        Console.WriteLine("numero invalido, porfavor digitar novaente");
+                    }
+                    else
+                    { Console.WriteLine($"nota {i} salva com sucesso..."); }
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"nota {i} = {n.Nota[i]}");
+                n.Media += n.Nota[i];
+            }
+
+            
+        }
+    }
+}
